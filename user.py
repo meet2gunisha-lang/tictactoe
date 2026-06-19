9485a962b8012553d9b2a62af111000f21a55d79
meet2gunisha-lang <meet2gunisha@gmail.com>
2026-06-19 16:12:54 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,10 @@
 import sqlite3
+current_user = None
+
+def set_current_user(username):
+    global current_user
+    current_user = username
+
 def create_database():
 
     conn = sqlite3.connect("nine_tiles.db")
@@ -8,8 +14,19 @@ def create_database():
     CREATE TABLE IF NOT EXISTS users(
         username TEXT PRIMARY KEY,
         password TEXT,
-        medium_wins INTEGER DEFAULT 0,
+        
+        easy_wins INTEGER DEFAULT 0,
+        easy_losses INTEGER DEFAULT 0,
+        easy_ties INTEGER DEFAULT 0,
+        
         medium_losses INTEGER DEFAULT 0,
+        medium_wins INTEGER DEFAULT 0,
+        medium_ties INTEGER DEFAULT 0,
+        
+        hard_wins INTEGER DEFAULT 0,
+        hard_losses INTEGER DEFAULT 0,
+        hard_ties INTEGER DEFAULT 0,
+
         games_played INTEGER DEFAULT 0
     )
     """)
@@ -62,11 +79,50 @@ def login_user(username,password):
     return user
 
 #winss
+def add_easy_win():
+
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET easy_wins = easy_wins + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+#losss
+def add_easy_loss():
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET easy_losses = easy_losses + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+
+#winss
 def add_medium_win():
 
-    global current_user
 
-    if current_user is None:
+    if not current_user:
         return
 
     conn = sqlite3.connect("nine_tiles.db")
@@ -85,9 +141,7 @@ def add_medium_win():
 #losss
 def add_medium_loss():
 
-    global current_user
-
-    if current_user is None:
+    if not current_user:
         return
 
     conn = sqlite3.connect("nine_tiles.db")
@@ -103,6 +157,111 @@ def add_medium_loss():
     conn.commit()
     conn.close()
 
+
+
+#winss
+def add_hard_win():
+
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET hard_wins = hard_wins + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+#losss
+def add_hard_loss():
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET hard_losses = hard_losses + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+
+
+#winss
+def add_easy_tie():
+
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET easy_ties = easy_ties + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+#losss
+def add_medium_ties():
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET medium_ties = medium_ties + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
+
+
+
+#winss
+def add_hard_ties():
+
+
+    if not current_user:
+        return
+
+    conn = sqlite3.connect("nine_tiles.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE users
+    SET hard_ties = hard_ties + 1,
+        games_played = games_played + 1
+    WHERE username = ?
+    """,(current_user,))
+
+    conn.commit()
+    conn.close()
+
 #leaderboard
 def get_top_players():
 
@@ -110,9 +269,12 @@ def get_top_players():
     cursor = conn.cursor()
 
     cursor.execute("""
-    SELECT username, medium_wins
+    SELECT username, medium_wins + easy_wins + hard_wins,
+        medium_losses + easy_losses + hard_losses,
+        medium_ties + easy_ties + hard_ties,
+        games_played
     FROM users
-    ORDER BY medium_wins DESC
+    ORDER BY medium_wins + easy_wins + hard_wins DESC
     LIMIT 3
     """)
 
